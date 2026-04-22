18833f56e05fa28cfc5c433f798fc91ab44270a2
Muaaz Majid <jacksporrow59@gmail.com>
2026-04-23 02:21:14 +0500

    Add files via upload

---
+++ Python Practice.py
@@ -0,0 +1,33 @@
+print("Hello World")
+print("     / |")
+print("    /  |")
+print("   /   |")
+print("  /____|")
+character_name="john"
+character_age= "35"
+print("His name is "+character_name+",His age is "+character_age)
+character_name="mike"
+character_age= 20
+is_Male=True
+print( character_name +" is a very good friend of mine and my age is "+ str(character_age))
+print(character_age) 
+character_balance= 50.23456
+print(character_balance)
+# second Lesson
+str= "Hello"
+print(str)
+print (str.upper())
+print (str.lower())
+print( len(str))
+print (str. isupper() )
+print (str[0])
+print(str.index("l")) #show the location
+print(str.replace("ll" , "LL")) # used for replacement
+#Working With Numbers
+NUM= 10
+print(abs(NUM)) # for absolute value
+print(pow(3, 2)) #Used for powers
+print (round(3.4999) ) #round off values
+from math import *
+print(floor(3.234))# used to show the lowest number and ceil is its opposite function
+print(sqrt(44))
