7d0c72fa87c0db4e0a64e87adb3ab5170b3a8431
Muaaz Majid <jacksporrow59@gmail.com>
2026-05-01 16:34:24 +0500

    Add files via upload

---
+++ guessing game .py
@@ -0,0 +1,8 @@
+print("Guess the Word")
+word ="Dog"
+guess= " "
+while guess != word:
+    guess= input("Enter the word (Hint: word is the name of a street animal starting with 'D' )")
+
+print ("You Win!!!!")
+    
